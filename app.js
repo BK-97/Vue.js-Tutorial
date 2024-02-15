70301f5a2db7e9429b4b3dd511d70a51b12b4e56
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-15 17:44:07 +0300

    open file

---
+++ app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+
+    data(){
+        return {
+            showBooks: true,
+            title: 'The Final Empire',
+            author: 'Brandon Sanderson',
+            age: 45,
+            x:0,
+            y:0
+        }
+    },
+    methods:{
+        toggleShowBooks(){
+            this.showBooks=!this.showBooks;
+        },
+        handleEvent(e,data){
+            console.log(e,e.type)
+            if(data){
+                console.log(data)
+            }
+        },
+        handleMousemove(e){
+            this.x=e.offsetX
+            this.y=e.offsetY
+        },
+        handleMouseExit(e){
+            this.x=0
+            this.y=0
+        }
+    }
+});
+
+app.mount('#app')
