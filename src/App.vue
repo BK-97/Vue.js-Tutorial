f201a7397913b4f989cc6394d30e20978bc3d672
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-19 01:48:35 +0300

    newProjectForm

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,14 @@
 <template>
-<h1>Reaction Timer</h1>
-<button @click="start" :disabled="isPlaying">Play</button>
-<Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-<Results v-if="showResult" :score="score"/>
+<SingupForm />
 </template>
 
 <script>
-import Block from './components/Block.vue'
-import Results from './components/Results.vue'
 
+import SingupForm from './components/SingupForm.vue';
 export default {
   name: 'App',
-  components:{Block,Results},
-  data(){
-    return {
-      isPlaying:false,
-      delay: null,
-      score: null,
-      showResult: false
-    }
-  },
-  methods:{
-    start(){
-      this.delay=2000+Math.random()*5000
-      this.isPlaying=true
-      this.showResult=false
-    },
-    endGame(reactionTime){
-      this.score=reactionTime
-      this.isPlaying=false
-      this.showResult=true
-    }
-  }
+  components:{SingupForm},
+  
 }
 
 </script>
@@ -45,20 +22,9 @@ export default {
   color: #444;
   margin-top: 60px;
 }
-button{
-  background: #0faf87;
-  color: white;
-  border: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-  font-size: 16px;
-  letter-spacing: 1px;
-  cursor: pointer;
-  margin: 10px;
-}
-button[disabled]{
-  opacity: 0.2;
-  cursor: not-allowed;
+body{
+  margin: 0;
+  background: #eee;
 }
 
 </style>
