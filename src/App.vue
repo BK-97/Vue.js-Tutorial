7357823fb36f3667ba3a34e05bda6b7a129d7973
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-19 01:03:43 +0300

    ProjectStart

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,14 @@
 <template>
-  <h1>{{title}}</h1>
-  <p>Welcome</p>
-  <teleport to=".modals" v-if="showModal">
-    <Modal theme="" @close="toggleModal">
-      <template v-slot:links>
-        <a href="#">sing up now</a>
-        <a href="#">more info</a>
-      </template>
-      <h1>Trendyol Giveaway</h1>
-      <p>Buy Shirts for half price</p>
-      </Modal>
-  </teleport>
-
-  <teleport to="#modalsid" v-if="showModalTwo">
-    <Modal @close="toggleModalTwo">
-      <h1>Hepsiburada Giveaway</h1>
-      <p>Buy Phone for half price</p>
-    </Modal>
-  </teleport>
-
-
-  <button @click="toggleModal">open modal</button>
-  <button @click="toggleModalTwo">open modal 2</button>
-
+<h1>Reaction Timer</h1>
 </template>
 
 <script>
-import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components:{Modal},
-  data(){
-    return {
-      title: 'My First Vue App :)',
-      showModal: false,
-      showModalTwo: false
-    }
-  },
-  methods: {
-    toggleModal(){
-      this.showModal=!this.showModal
-    },
-    toggleModalTwo(){
-      this.showModalTwo=!this.showModalTwo
-    }
-  }
+  components:{}
 }
+
 </script>
 
 <style>
@@ -55,12 +17,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
-h1{
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
-}
+
 </style>
