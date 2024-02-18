c9eae82f810aa05d2ba1658e9bbcfaea68379f1b
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-19 01:30:47 +0300

    gameLoop

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <template>
 <h1>Reaction Timer</h1>
+<button @click="start" :disabled="isPlaying">Play</button>
+<Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+<p v-if="showResult">Reaction Time: {{ score }} ms</p>
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
 export default {
   name: 'App',
-  components:{}
+  components:{Block},
+  data(){
+    return {
+      isPlaying:false,
+      delay: null,
+      score: null,
+      showResult: false
+    }
+  },
+  methods:{
+    start(){
+      this.delay=2000+Math.random()*5000
+      this.isPlaying=true
+      this.showResult=false
+    },
+    endGame(reactionTime){
+      this.score=reactionTime
+      this.isPlaying=false
+      this.showResult=true
+    }
+  }
 }
 
 </script>
