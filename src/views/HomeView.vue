220bb5f84045065e780da91857ed9f0a230ecdd0
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-19 15:09:36 +0300

    routerLinks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam obcaecati voluptates hic fuga consequatur eos officiis eveniet tenetur aliquid quae impedit iusto temporibus culpa eius quibusdam, rem inventore doloremque facere voluptas repellendus. Totam laborum, repellat in odio suscipit accusantium labore quas libero. In eum dolor sit velit, error minus aliquid. Distinctio doloribus cupiditate suscipit sapiente odit labore vero, animi eum dignissimos veniam alias quisquam. Quibusdam neque molestiae dolorum commodi non, delectus alias ducimus rerum deserunt recusandae? Fuga iste omnis aperiam id cupiditate similique laboriosam eaque! Labore nostrum quidem reprehenderit eius beatae modi ea rerum culpa, iste voluptates eligendi totam omnis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam obcaecati voluptates hic fuga consequatur eos officiis eveniet tenetur aliquid quae impedit iusto temporibus culpa eius quibusdam, rem inventore doloremque facere voluptas repellendus. Totam laborum, repellat in odio suscipit accusantium labore quas libero. In eum dolor sit velit, error minus aliquid. Distinctio doloribus cupiditate suscipit sapiente odit labore vero, animi eum dignissimos veniam alias quisquam. Quibusdam neque molestiae dolorum commodi non, delectus alias ducimus rerum deserunt recusandae? Fuga iste omnis aperiam id cupiditate similique laboriosam eaque! Labore nostrum quidem reprehenderit eius beatae modi ea rerum culpa, iste voluptates eligendi totam omnis.</p>
+  
   </div>
 </template>
 
