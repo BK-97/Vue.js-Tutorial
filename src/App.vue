1b87bef30205097226f90b29ca5f0a821dcf72a6
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-19 13:39:53 +0300

    newProject

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-<SingupForm />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-import SingupForm from './components/SingupForm.vue';
-export default {
-  name: 'App',
-  components:{SingupForm},
-  
-}
-
-</script>
-
 <style>
-#app, .modals,#modalsid {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #444;
-  margin-top: 60px;
-}
-body{
-  margin: 0;
-  background: #eee;
+  color: #2c3e50;
 }
 
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
