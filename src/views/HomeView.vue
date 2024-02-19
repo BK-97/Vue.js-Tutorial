63714f737a13374261eeb0494d9379dc4616850a
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-20 00:41:35 +0300

    composition basics

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="home">Home
+    <p>My name is {{  name }} and my age is {{ age }}</p>
+    <button @click="handleclick"> click me</button>
   </div>
 </template>
-
 <script>
 
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  setup(){
+    let name='mario'
+    let age=30
+
+    const handleclick = () =>{
+      console.log('you clicked me')
+    }
+    return{
+      name,age,handleclick
+    }
+  }
 } 
 </script>
