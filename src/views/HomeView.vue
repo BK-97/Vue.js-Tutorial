b3d453533708b95b8255fe48fa9b3e93cb3d70ce
Burak Kocatürk <59361739+BK-97@users.noreply.github.com>
2024-02-24 22:08:44 +0300

    PropsInSetup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-
-    <button @click="handleClick">Stop watching</button>
+    <PostList :posts="posts"/>
   </div>
 </template>
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
   name: 'HomeView',
+  components:{PostList},
   setup(){
-    const search=ref('')
-    const names=ref(['mario','yoshi','luigi','toad'])
-
-    const stopwatch=watch(search,()=>{
-      console.log('watch function run')
-    })
-    const stopwatchEffect=watchEffect(()=>{
-      console.log('watch effect function run',search.value)
-
-    })
-    const matchingNames=computed(()=>{
-      return names.value.filter((name)=> name.includes(search.value))
-    })
-    const handleClick=()=>{
-      stopwatch()
-      stopwatchEffect()
-    }
-    return{names,search,matchingNames,handleClick}
+    const posts=ref([
+      {title:'Welcome to the blog',body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',id:1},
+      {title:'top 5 CSS tips',body:'Lorem ipsum',id:2},
+    ])
+    return{posts}
   }
 } 
 </script>
